c49ad92b1c960fdd9bd831b144be701c326901aa
rami saleem <icreativeapp@gmail.com>
2013-02-16 15:40:14 +0300

    Fix bad syntax

--- src/test/java/RegisterWithInvalidEmail.java
+++ src/test/java/RegisterWithInvalidEmail.java
@@ -95,7 +95,7 @@ public class RegisterWithInvalidEmail implements SauceOnDemandSessionIdProvider,
     @Test
     public void Register_With_Invalid_Email() throws Exception {
 
-        ddriver.get("http://markavip.com");
+        driver.get("http://markavip.com");
 	driver.findElement(By.className("do_modal")).click();
 	WebDriverWait wait=new WebDriverWait(driver, 25);
 	wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("fb-login-section")));
